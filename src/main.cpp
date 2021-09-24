7ccf36dc78d6425b7dd0fcf2d6828d7c2c21e159
Niklas Mäckle <niklas.maeckle@gmail.com>
2021-09-25 01:13:33 +0200

    slight progress with .vox file loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #define STB_IMAGE_IMPLEMENTATION
+#define OGT_VOX_IMPLEMENTATION
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -16,7 +17,15 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include "ownImgui.h"
 #include "lightValues.h"
-//#include <stdio.h>
+
+#include "ogt_vox.h"
+#include "ogt_voxel_meshify.h"
+
+#if defined(_MSC_VER)
+#include <io.h>
+#endif
+#include <stdio.h>
+
 //#include <cubeLight.h>
 
 //#include <imgui/backends/imgui_impl_opengl3.h>
@@ -30,7 +39,14 @@ void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 void keyboard_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 void processInput(GLFWwindow *window);
+
 unsigned int loadTexture(char const *path);
+
+const ogt_vox_scene *load_vox_scene(const char *filename, uint32_t scene_read_flags = 0);
+const ogt_vox_scene *load_vox_scene_with_groups(const char *filename);
+uint32_t count_solid_voxels_in_model(const ogt_vox_model *model);
+void save_vox_scene(const char *pcFilename, const ogt_vox_scene *scene);
+std::vector<glm::vec3> getVoxelPositions(const ogt_vox_model *model);
 
 // settings
 const unsigned int SCR_WIDTH = 1600;
@@ -197,7 +213,40 @@ int main()
     //cubeLights.addLight(pointLightPositions[0]);
 
     LightValues lightValues;
+    std::vector<std::vector<glm::vec3>> model_positions;
 
+    const ogt_vox_scene *scene = load_vox_scene("res/vox/Ruriko-ji Temple Pagoda.vox");
+    std::vector<ogt_mesh*> meshes;
+    if (scene)
+    {
+        //std::vector<glm::vec3> voxel_positions;
+        for (uint32_t model_index = 0; model_index < scene->num_models; model_index++)
+        {
+            const ogt_vox_model *model = scene->models[model_index];
+
+            ogt_voxel_meshify_context ctx;
+            memset(&ctx, 0, sizeof(ctx));
+            ogt_mesh* mesh = ogt_mesh_from_paletted_voxels_simple(&ctx, model->voxel_data, model->size_x, model->size_y, model->size_z, (const ogt_mesh_rgba*)&scene->palette.color[0]);
+            meshes.push_back(mesh);
+
+            /*
+            std::vector<glm::vec3> voxel_positions = getVoxelPositions(model);
+            model_positions.push_back(voxel_positions);
+            uint32_t total_voxel_count = model->size_x * model->size_y * model->size_z;
+
+            printf(" model[%u] has dimension %ux%ux%u, with %u solid voxels of the total %u voxels (hash=%u)!\n",
+                   model_index,
+                   model->size_x, model->size_y, model->size_z,
+                   total_voxel_count,
+                   model->voxel_hash);
+                   */
+        }
+
+        //save_vox_scene("saved.vox", scene);
+
+        ogt_vox_destroy_scene(scene);
+
+    }
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -252,65 +301,6 @@ int main()
         lightingShader.use();
         lightingShader.setVec3("viewPos", camera.Position);
 
-        // directional light
-        /*        lightingShader.setVec3("dirLight.direction", dirDirection.x, dirDirection.y, dirDirection.z);
-        lightingShader.setVec3("dirLight.ambient", dirAmbient.x, dirAmbient.y, dirAmbient.z);
-        lightingShader.setVec3("dirLight.diffuse", dirDiffuse.x, dirDiffuse.y, dirDiffuse.z);
-        lightingShader.setVec3("dirLight.specular", dirSpecular.x, dirSpecular.y, dirSpecular.z);
-        // point light 1
-        lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
-        lightingShader.setVec3("pointLights[0].ambient", pointAmbient.x, pointAmbient.y, pointAmbient.z);
-        lightingShader.setVec3("pointLights[0].diffuse", pointDiffuse.x, pointDiffuse.y, pointDiffuse.z);
-        lightingShader.setVec3("pointLights[0].specular", pointSpecular.x, pointSpecular.y, pointSpecular.z);
-        lightingShader.setFloat("pointLights[0].constant", 1.0f);
-        lightingShader.setFloat("pointLights[0].linear", pointLinear);
-        lightingShader.setFloat("pointLights[0].quadratic", pointQuadratic);
-        // point light 2
-        lightingShader.setVec3("pointLights[1].position", pointLightPositions[1]);
-        lightingShader.setVec3("pointLights[1].ambient", pointAmbient.x, pointAmbient.y, pointAmbient.z);
-        lightingShader.setVec3("pointLights[1].diffuse", pointDiffuse.x, pointDiffuse.y, pointDiffuse.z);
-        lightingShader.setVec3("pointLights[1].specular", pointSpecular.x, pointSpecular.y, pointSpecular.z);
-        lightingShader.setFloat("pointLights[1].constant", 1.0f);
-        lightingShader.setFloat("pointLights[1].linear", pointLinear);
-        lightingShader.setFloat("pointLights[1].quadratic", pointQuadratic);
-        // point light 3
-        lightingShader.setVec3("pointLights[2].position", pointLightPositions[2]);
-        lightingShader.setVec3("pointLights[2].ambient", pointAmbient.x, pointAmbient.y, pointAmbient.z);
-        lightingShader.setVec3("pointLights[2].diffuse", pointDiffuse.x, pointDiffuse.y, pointDiffuse.z);
-        lightingShader.setVec3("pointLights[2].specular", pointSpecular.x, pointSpecular.y, pointSpecular.z);
-        lightingShader.setFloat("pointLights[2].constant", 1.0f);
-        lightingShader.setFloat("pointLights[2].linear", pointLinear);
-        lightingShader.setFloat("pointLights[2].quadratic", pointQuadratic);
-        // point light 4
-        lightingShader.setVec3("pointLights[3].position", pointLightPositions[3]);
-        lightingShader.setVec3("pointLights[3].ambient", pointAmbient.x, pointAmbient.y, pointAmbient.z);
-        lightingShader.setVec3("pointLights[3].diffuse", pointDiffuse.x, pointDiffuse.y, pointDiffuse.z);
-        lightingShader.setVec3("pointLights[3].specular", pointSpecular.x, pointSpecular.y, pointSpecular.z);
-        lightingShader.setFloat("pointLights[3].constant", 1.0f);
-        lightingShader.setFloat("pointLights[3].linear", pointLinear);
-        lightingShader.setFloat("pointLights[3].quadratic", pointQuadratic);
-        //cubeLights.setLightValues(0, lightingShader);
-        // spotLight
-        lightingShader.setVec3("spotLight.position", camera.Position);
-        lightingShader.setVec3("spotLight.direction", camera.Front);
-        lightingShader.setVec3("spotLight.ambient", spotAmbient.x, spotAmbient.y, spotAmbient.z);
-        lightingShader.setVec3("spotLight.diffuse", spotDiffuse.x, spotDiffuse.y, spotDiffuse.z);
-        lightingShader.setVec3("spotLight.specular", spotSpecular.x, spotSpecular.y, spotSpecular.z);
-
-        lightingShader.setFloat("spotLight.constant", 1.0f);
-        lightingShader.setFloat("spotLight.linear", spotLinear);
-        lightingShader.setFloat("spotLight.quadratic", spotQuadratic);
-        if(showSpotlight)
-        {
-            lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(spotCutOff)));
-            lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(spotOuterCutOff))); 
-        }
-        else
-        {
-            lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(0.0f)));
-            lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(0.0f))); 
-        }
-        */
         lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
         lightingShader.setVec3("pointLights[1].position", pointLightPositions[1]);
         lightingShader.setVec3("pointLights[2].position", pointLightPositions[2]);
@@ -333,6 +323,31 @@ int main()
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
         lightingShader.setMat4("model", model);
         ourModel.Draw(lightingShader);
+
+        for (unsigned int i = 0; i < meshes.size(); ++i)
+        {
+            ogt_mesh mesh = &meshes[i];
+            glBegin(GL_TRIANGLES);
+        for (uint32_t i = 0; i < mesh->index_count; i+=3)
+        {
+          uint32_t i0 = mesh->indices[i + 0];
+          uint32_t i1 = mesh->indices[i + 1];
+          uint32_t i2 = mesh->indices[i + 2];
+          const ogt_mesh_vertex* v0 = &mesh->vertices[i0];
+          const ogt_mesh_vertex* v1 = &mesh->vertices[i1];
+          const ogt_mesh_vertex* v2 = &mesh->vertices[i2];
+          glColor4ubv(&v0->color);
+          glNormal3fv(&v0->normal);
+          glVertex3fv(&v0->pos);
+          glColor4ubv(&v1->color);
+          glNormal3fv(&v1->normal);
+          glVertex3fv(&v1->pos);
+          glColor4ubv(&v2->color);
+          glNormal3fv(&v2->normal);
+          glVertex3fv(&v2->pos);
+        }
+        glEnd();
+        }
 
         // also draw the lamp objects
         lightCubeShader.use();
@@ -504,4 +519,115 @@ unsigned int loadTexture(char const *path)
     }
 
     return textureID;
+}
+
+const ogt_vox_scene *load_vox_scene(const char *filename, uint32_t scene_read_flags)
+{
+    // open the file
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+    FILE *fp;
+    if (0 != fopen_s(&fp, filename, "rb"))
+        fp = 0;
+#else
+    FILE *fp = fopen(filename, "rb");
+#endif
+    if (!fp)
+        return NULL;
+
+    // get the buffer size which matches the size of the file
+    fseek(fp, 0, SEEK_END);
+    uint32_t buffer_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    // load the file into a memory buffer
+    uint8_t *buffer = new uint8_t[buffer_size];
+    fread(buffer, buffer_size, 1, fp);
+    fclose(fp);
+
+    // construct the scene from the buffer
+    const ogt_vox_scene *scene = ogt_vox_read_scene_with_flags(buffer, buffer_size, scene_read_flags);
+
+    // the buffer can be safely deleted once the scene is instantiated
+    delete[] buffer;
+
+    return scene;
+}
+
+const ogt_vox_scene *load_vox_scene_with_groups(const char *filename)
+{
+    return load_vox_scene(filename, k_read_scene_flags_groups);
+}
+
+std::vector<glm::vec3> getVoxelPositions(const ogt_vox_model *model)
+{
+    std::vector<glm::vec3> voxel_positions;
+    uint32_t voxel_index = 0;
+    //uint8_t counter = 0;
+    for (uint32_t z = 0; z < model->size_z; z++)
+    {
+        for (uint32_t y = 0; y < model->size_y; y++)
+        {
+            for (uint32_t x = 0; x < model->size_x; x++, voxel_index++)
+            {
+                // if color index == 0, this voxel is empty, otherwise it is solid.
+                uint32_t color_index = model->voxel_data[voxel_index];
+                if (color_index != 0)
+                    voxel_positions.push_back(glm::vec3(x, y, z));
+            }
+        }
+    }
+    return voxel_positions;
+}
+/*uint32_t count_solid_voxels_in_model(const ogt_vox_model *model)
+{
+    uint32_t solid_voxel_count = 0;
+    uint32_t voxel_index = 0;
+    //uint8_t counter = 0;
+    for (uint32_t z = 0; z < model->size_z; z++)
+    {
+        for (uint32_t y = 0; y < model->size_y; y++)
+        {
+            for (uint32_t x = 0; x < model->size_x; x++, voxel_index++)
+            {
+                // if color index == 0, this voxel is empty, otherwise it is solid.
+                uint32_t color_index = model->voxel_data[voxel_index];
+                /*if(color_index != 0)
+                    std::cout << color_index << ' ';
+                if(counter >= 10)
+                    std::cout << std::endl;*/
+/*
+                bool is_voxel_solid = (color_index != 0);
+                // add to our accumulator
+                solid_voxel_count += (is_voxel_solid ? 1 : 0);
+            }
+        }
+    }
+    return solid_voxel_count;
+}*/
+
+void save_vox_scene(const char *pcFilename, const ogt_vox_scene *scene)
+{
+    // save the scene back out.
+    uint32_t buffersize = 0;
+    uint8_t *buffer = ogt_vox_write_scene(scene, &buffersize);
+    if (!buffer)
+        return;
+
+        // open the file for write
+#if defined(_MSC_VER) && _MSC_VER >= 1400
+    FILE *fp;
+    if (0 != fopen_s(&fp, pcFilename, "wb"))
+        fp = 0;
+#else
+    FILE *fp = fopen(pcFilename, "wb");
+#endif
+    if (!fp)
+    {
+        ogt_vox_free(buffer);
+        return;
+    }
+
+    fwrite(buffer, buffersize, 1, fp);
+    fclose(fp);
+    ogt_vox_free(buffer);
 }
